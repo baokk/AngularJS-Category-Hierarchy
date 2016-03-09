5970ba4ec10074e747f89b3754caaa5f97a67b8b
khiemnvd <khiemnvd.dev@gmail.com>
2016-03-09 16:00:50 +0700

    add code encrypt a string

---
+++ Blog.Common/Encrypt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Common
+{
+    public class Encrypt
+    {
+        public static string EncryptString(string str)
+        {
+            HashAlgorithm hash = new SHA256Managed();
+            var plainTextBytes = Encoding.UTF8.GetBytes(str);
+            var hashBytes = Convert.ToBase64String(hash.ComputeHash(plainTextBytes));
+
+            return hashBytes;
+        }
+    }
+}
