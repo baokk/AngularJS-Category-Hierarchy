219b49657f4bccbcb2e1f6d6711eb4d7ed2a46af
khiemnvd <khiemnvd.dev@gmail.com>
2016-04-04 11:57:04 +0700

    add code category slug

--- Blog.WebAPI/app/admin/components/category/categoryController.js
+++ Blog.WebAPI/app/admin/components/category/categoryController.js
@@ -29,38 +29,43 @@
             }, true);
 
             // GET parent categories
-    $scope.getParentCategory = function (category) {
-        var promiseGetSingle = categoryService.getCategory(category.category_id);
-        promiseGetSingle.then(function(pl) {
-            var res = pl.data;
-            $scope.category_id = res.category_id;
-            $scope.category_name = res.category_name;
-            $scope.category_slug = res.category_slug;
-            $scope.category_description = res.category_description,
-                $scope.category_parent = res.category_parent;
-            $scope.category_active = res.category_active;
+            $scope.getParentCategory = function (category) {
+                var promiseGetSingle = categoryService.getCategory(category.category_id);
+                promiseGetSingle.then(function (pl) {
+                    var res = pl.data;
+                    $scope.category_id = res.category_id;
+                    $scope.category_name = res.category_name;
+                    $scope.category_slug = res.category_slug;
+                    $scope.category_description = res.category_description,
+                        $scope.category_parent = res.category_parent;
+                    $scope.category_active = res.category_active;
 
-        }, function(error) {
-            console.log('message error: ' + error);
-        });
-    }
+                }, function (error) {
+                    console.log("message error: " + error);
+                });
+            }; 
+            // GET children categories
+            $scope.getChildrenCategory = function (children) {
+                var promiseGetSingle = categoryService.getCategory(children.category_id);
+                promiseGetSingle.then(function (pl) {
+                    var res = pl.data;
+                    $scope.category_id = res.category_id;
+                    $scope.category_name = res.category_name;
+                    $scope.category_slug = res.category_slug;
+                    $scope.category_description = res.category_description,
+                    $scope.category_parent = res.category_parent;
+                    $scope.category_active = res.category_active;
 
-    // GET children categories
-    $scope.getChildrenCategory = function (children) {
-        var promiseGetSingle = categoryService.getCategory(children.category_id);
-        promiseGetSingle.then(function(pl) {
-            var res = pl.data;
-            $scope.category_id = res.category_id;
-            $scope.category_name = res.category_name;
-            $scope.category_slug = res.category_slug;
-            $scope.category_description = res.category_description,
-                $scope.category_parent = res.category_parent;
-            $scope.category_active = res.category_active;
+                }, function (error) {
+                    console.log("message error: " + error);
+                });
+            };
 
-        }, function(error) {
-            console.log('message error: ' + error);
-        });
-    }
-
+            //$scope.domainName = "http://" + document.domain.toString() + "/category/";
+            //// Replace blank with underscore
+            $scope.categoryNameOnBlur = function() {
+                var slug = ($scope.category.category_name).toString().replace(/_| /g, "-").toLowerCase();
+                $scope.category_slug = slug;
+            }
         }]);
 })();
--- Blog.WebAPI/app/admin/components/category/categoryService.js
+++ Blog.WebAPI/app/admin/components/category/categoryService.js
@@ -3,26 +3,20 @@
     
     app.factory("categoryService", function($http) {
 
-        var baseUrl = "api/Category/"
+        var baseUrl = "api/Category/";
         var factory = {};
         
         //GET all categories
         factory.getCategories = function() {
             return $http.get(baseUrl);
-        }
-
-        // GET single category
+        }; // GET single category
         factory.getCategory = function (category_id) {
             return $http.get(baseUrl + category_id);
-        }
-
-        // INSERT a new category
+        }; // INSERT a new category
         factory.createCategory = function (category) {
             return $http.post(baseUrl, category);
-        }
-
+        };
         return factory;
 
-    })
-
+    });
 })();
