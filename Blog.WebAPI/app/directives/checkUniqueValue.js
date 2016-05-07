3e287584fcace2758992f1d955796e59ccc7ca74
Khiem Nguyen <khiemnvd@hotmail.com>
2016-05-07 23:53:07 +0700

    remove project blog.web

---
+++ Blog.WebAPI/app/directives/checkUniqueValue.js
@@ -0,0 +1,19 @@
+﻿app.directive('checkValueExists', ['userService', function(userService){
+   return directive = {
+       restrict: 'A',
+       require: 'ngModel',
+       link: function(scope, element, attrs, ngModel){
+           element.on('blur', function(evt){
+               if(!ngModel || !element.val())
+                   return;
+               var currentValue = element.val();
+               userService.checkUserNameExists(currentValue)
+               .then(function (response){
+                   ngModel.$setValidity('unique', response);
+               }, function(){
+                   ngModel.$setValidity('unique', true);
+               })
+           })
+       }
+   }
+}]);
